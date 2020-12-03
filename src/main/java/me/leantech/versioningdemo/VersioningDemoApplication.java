4b622c4e8f2083200088477d8d8adb74d0c8f166
Illia Dubinin <illia@leantech.me>
2020-12-03 13:04:00 +0000

    Project generated by start.spring.io

---
+++ src/main/java/me/leantech/versioningdemo/VersioningDemoApplication.java
@@ -0,0 +1,13 @@
+package me.leantech.versioningdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersioningDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersioningDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/leantech/versioningdemo/VersioningDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.leantech.versioningdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersioningDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
